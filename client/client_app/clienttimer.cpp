b35e23767683ef2d412966b134e1c9f833429517
Johny <j.dudziak97@gmail.com>
2019-01-08 18:31:52 +0100

    Add timer in client

---
+++ client/client_app/clienttimer.cpp
@@ -0,0 +1,35 @@
+#include "clienttimer.h"
+#include <QtCore>
+
+clientTimer::clientTimer()
+{
+    timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(modifyTime()));
+}
+
+int clientTimer::getTime(){
+    return time;
+}
+
+void clientTimer::setTime(int t){
+    time = t;
+    startTime();
+    emit changeTime();
+}
+
+void clientTimer::startTime(){
+    timer->start(1000);
+}
+
+void clientTimer::stopTime(){
+    timer->stop();
+}
+
+void clientTimer::modifyTime(){
+    time = time - 1;
+    if(time == 0){
+        stopTime();
+    }
+    emit changeTime();
+}
+
