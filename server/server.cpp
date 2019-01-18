981004944e331d90c51317526265782f2e7ecc05
Johny <j.dudziak97@gmail.com>
2019-01-18 20:35:35 +0100

    Font size in answers changed and code clean up in project.

--- server/server.cpp
+++ server/server.cpp
@@ -17,15 +17,11 @@
 
 int main(int argc, char ** argv)
 {
-    // get and validate port number
     if(argc != 3) error(1, 0, "Need config file and questions file paths");
     
-    // // graceful ctrl+c exit
-    // signal(SIGINT, ctrl_c); //TODO
     // prevent dead sockets from raising pipe signals on write
     signal(SIGPIPE, SIG_IGN);
 
-    //TODO wczytanie parametrów z pliku albo argv
-    GameServer gameServer(argv[1], argv[2]); //num of questions and time per question
+    GameServer gameServer(argv[1], argv[2]);
     gameServer.run();
 }
